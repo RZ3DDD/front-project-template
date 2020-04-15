1deb50ac9871e2af3c9f9381438d9bd73d9d96e9
Yury Shchutsky <rz3ddd@gmail.com>
2020-04-15 19:03:54 +0300

    gulpfile.js - Added gulp-babel and sourcemaps for scripts function.

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,25 @@
 /*jshint esversion: 6 */
 
 // Подключаем необходимые модули
-const   {series,
-         parallel,
-         src,
-         dest,
-         watch
-        } = require('gulp');
+const {
+    series,
+    parallel,
+    src,
+    dest,
+    watch
+} = require('gulp');
 
-const concat       = require('gulp-concat');
-const autoprefixer = require('gulp-autoprefixer'); 
-const cleanCSS     = require('gulp-clean-css');
-const uglify       = require('gulp-uglify');
-const del          = require('del');
-const browserSync  = require('browser-sync').create();
-const sourcemaps   = require('gulp-sourcemaps');
-const sass         = require('gulp-sass');
-const imagemin     = require('gulp-imagemin');
-const rename       = require('gulp-rename');
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const babel = require("gulp-babel");
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
+const rename = require('gulp-rename');
 
 
 // Private task подготовки CSS-определений к развёртыванию 
@@ -32,23 +34,24 @@ const cssFiles = [
     // "./src/css/sass-to.css",
     "./src/css/**/*.css"
 ];
+
 function styles() {
     return src(sassFiles, cssFiles)
-    .pipe(sourcemaps.init())
-    .pipe(sass())
-    .pipe(concat("styles.css"))
-    .pipe(autoprefixer({
-        cascade: false
-    }))
-    .pipe(cleanCSS({
-        level: 2
-    }))
-    .pipe(rename({
-        suffix: '.min'
-    }))
-    .pipe(sourcemaps.write('./'))
-    .pipe(dest("./build/css"))
-    .pipe(browserSync.stream()); // auto-inject into browsers
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(concat("styles.css"))
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        .pipe(cleanCSS({
+            level: 2
+        }))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(dest("./build/css"))
+        .pipe(browserSync.stream()); // auto-inject into browsers
 }
 
 // jsFiles привязка указывающая на порядок объединения файлов js
@@ -61,24 +64,29 @@ const jsFiles = [
 // Private task подготовки JS-скриптов к развёртыванию 
 function scripts() {
     return src(jsFiles)
-    .pipe(concat("scripts.js"))
-    .pipe(uglify({
-        toplevel: true
-    }))
-    .pipe(rename({
-        suffix: '.min'
-    }))
-    .pipe(dest("./build/js"))
-    .pipe(browserSync.stream()); // auto-inject into browsers
+        .pipe(concat("scripts.js"))
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(uglify({
+            toplevel: true
+        }))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(sourcemaps.write('maps'))
+        .pipe(dest("./build/js"))
+        .pipe(browserSync.stream()); // auto-inject into browsers
 }
 
 // Private task сжатия изображений
 function imgCompress() {
     return src('./src/img/**')
-    .pipe(imagemin({
-        progressive: true
-    }))
-    .pipe(dest('./build/img/'));
+        .pipe(imagemin({
+            progressive: true
+        }))
+        .pipe(dest('./build/img/'));
 }
 
 // Private task очистки папки build
@@ -97,7 +105,7 @@ function overwatch() {
     watch('./src/css/**/*.css', styles); // Следить за CSS файлами
     watch('./src/sass/**/*.scss', styles); // Следить за CSS файлами
     watch('./src/sass/**/*.sass', styles); // Следить за CSS файлами
-    watch('./src/js/**/*.js', scripts);  // Следить за JS файлами
+    watch('./src/js/**/*.js', scripts); // Следить за JS файлами
     watch('./*.html').on('change', browserSync.reload); // Следить за изменением HTML файлов
 }
 
